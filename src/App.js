f22e40a7123b544d88b0253f221e101a46553c58
trusha.nimse <trushanimse07@gmail.com>
2025-08-19 17:04:45 +0530

    even odd

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let arr = [10, 20, 30, 40, 50]
+  let arr = [10, 25, 30, 45, 50]
   
   return (
     <div>
-      <div>{arr}</div>
-      <div>
-     {arr.map((singleElement)=>{
-      return <div>{singleElement*2}</div>
-    
-     })} 
-    </div>
+      {arr.map((singleElement)=>{
+        if(singleElement%2===0){
+          return<div><i>{singleElement*2}</i></div>
+        }
+        else{
+          return<b>{singleElement*2}</b>
+        }
+      })}
     </div>
       );
 }
