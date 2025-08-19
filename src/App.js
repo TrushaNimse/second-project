006f31545debd0add0ae513bb073d8c49fad7eaa
trusha.nimse <trushanimse07@gmail.com>
2025-08-19 16:54:44 +0530

    display array

--- src/App.js
+++ src/App.js
@@ -2,35 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let num1 = 12, num2 = 16;
-  let result = 0;
-
-function handleAddition(){
-  result=num1+num2;
-  console.log(result);
-}
-function handleSubtraction(){
-  result=num1-num2;
-  console.log(result);
-}
-function handleDivision(){
-  result=num1%num2;
-  console.log(result);
-}
-function handleMultiplication(){
-  result=num1*num2;
-  console.log(result);
-}
+  let arr = [10, 20, 30, 40, 50]
+  
   return (
     <div>
-    <div>{num1}</div>
-    <div>{num2}</div>  
-      <button onClick={handleAddition}>Addtion</button><br />
-      <button onClick={handleSubtraction}>Subtraction</button><br />
-      <button onClick={handleDivision}>Division</button><br />
-      <button onClick={handleMultiplication}>Multiplication</button><br />
+      <div>{arr}</div>
+      <div>
+     {arr.map((singleElement)=>{
+      return <div>{singleElement*2}</div>
+    
+     })} 
     </div>
-  );
+    </div>
+      );
 }
 
 export default App;
