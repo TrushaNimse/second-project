2b6628ad5dae6511fe8122a260676f7dea374c5f
trusha.nimse <trushanimse07@gmail.com>
2025-08-21 15:10:10 +0530

    multiplcation-table

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 
 function App() {
   let arr = [10, 25, 30, 45, 50]
+  let num=[1,2,3,4,5,6,7,8,9,10]
   
   return (
-    <div>
-      {arr.map((singleElement)=>{
-        if(singleElement%2===0){
-          return<div><i>{singleElement*2}</i></div>
-        }
-        else{
-          return<b>{singleElement*2}</b>
-        }
-      })}
-    </div>
+  
+  <div>{arr.map((singleElement)=>{
+     return <div> {num.map((singleTable)=>{
+      return<span>{singleElement*singleTable +"  "}</span>  
+      })}</div>
+    })}
+  </div>
       );
 }
 
