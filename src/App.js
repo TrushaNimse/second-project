87bd716faacdc63dabf7032b8dc30673530e6a1e
trusha.nimse <trushanimse07@gmail.com>
2025-08-23 12:23:17 +0530

    multiple operations on user number

--- src/App.js
+++ src/App.js
@@ -16,22 +16,43 @@ function App() {
   function handleNum2Change(event) {
     setNumber2(parseInt(event.target.value))
   }
-  function onSubmit(){
-    setAddition(number1+number2);
-    setSubtraction(number1-number2);
-    setMultiplication(number1*number2);
-    setDivision(number1/number2);
+  function onSubmit1() {
+    setAddition(number1 + number2);
+    setSubtraction(undefined);
+    setMultiplication(undefined);
+    setDivision(undefined);
+  }
+  function onSubmit2() {
+    setSubtraction(number1 - number2);
+    setAddition(undefined);
+    setMultiplication(undefined);
+    setDivision(undefined);
+  }
+  function onSubmit3() {
+    setMultiplication(number1 * number2);
+    setAddition(undefined);
+    setSubtraction(undefined);
+    setDivision(undefined);
+  }
+  function onSubmit4() {
+    setDivision(number1 / number2);
+    setAddition(undefined);
+    setSubtraction(undefined);
+    setMultiplication(undefined);
   }
   return (
     <div>
       <div><input name="number1" value={number1} onChange={handleNum1Change} placeholder='Enter 1st number' /></div>
       <div><input name="number2" value={number2} onChange={handleNum2Change} placeholder='Enter 2nd number' /></div>
-      <div><button onClick={onSubmit}>Enter</button></div>
-      {addition &&<div>Addition of {number1} and {number2} is : {addition}</div>}
-      {subtraction &&<div>Subtraction of {number1} and {number2} is : {subtraction}</div>}
-      {multiplication &&<div>Multiplication of {number1} and {number2} is : {multiplication}</div>}
-      {division&&<div>Division of {number1} and {number2} is : {division}</div>}
 
+      <button onClick={onSubmit1}>Addition</button>
+      <button onClick={onSubmit2}>Subtraction</button>
+      <button onClick={onSubmit3}>Multiplication</button>
+      <button onClick={onSubmit4}>Division</button>
+      {addition !==undefined && <div>Addition of {number1} and {number2} is : {addition}</div>}
+      {subtraction !==undefined && <div>Subtraction of {number1} and {number2} is : {subtraction}</div>}
+      {multiplication !==undefined && <div>Multiplication of {number1} and {number2} is : {multiplication}</div>}
+      {division !==undefined && <div>Division of {number1} and {number2} is : {division}</div>}
     </div>
   );
 }
